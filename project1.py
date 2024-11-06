f2701c52c95f0937df35daae11656a01d488e451
priyavatsavai <priyavatsavai997@gmail.com>
2024-11-06 22:06:30 +0530

    added few charts

--- project1.py
+++ project1.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import pandas as pd
 
 st.title('My first Streamlit app')
 st.write('Hello, Streamlit!')
+
+
+option = st.sidebar.selectbox('Select one option:', ('Option 1', 'Option 2'))
+st.write('You selected:', option)
+
+df = pd.read_csv('data.csv')
+# Convert date to datetime format
+df['Date'] = pd.to_datetime(df['Date'])
+# Extract year and month
+df['year'] = df['Date'].dt.year
+df['month'] = df['Date'].dt.month
+
+st.dataframe(df)
+
+dfone = df[["month", "Amount"]]
+dftwo = dfone.groupby("month").mean()
+st.bar_chart(dftwo)
